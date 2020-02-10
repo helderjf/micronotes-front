76b8dbae7b2d1a2cd0a1e179b963e492a4fd5636
Helder Faria <helderjf@gmail.com>
2020-02-10 21:42:51 +0000

    Update Notes view

--- src/app/notes/note-card/note-card.component.ts
+++ src/app/notes/note-card/note-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Note } from '../note';
 
 @Component({
   selector: 'app-note-card',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note-card.component.css']
 })
 export class NoteCardComponent implements OnInit {
+
+  @Input() note: Note;
 
   constructor() { }
 
